82952895ed205c54b2040d1e4ca44aab1c932814
amelia-liston <aliston@bu.edu>
2024-10-14 14:48:40 -0400

    Added docstrings and removed unnecessary imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, url_for
+from flask import Flask, request
 from flask_cors import CORS
 from scanner import score_text_contrast
 from textscanner import score_text_accessibility
@@ -11,18 +11,30 @@ cors = CORS(
 
 @app.route("/")
 def home():
-    # # CHANGE AFTER DOMAINS HAVE BEEN ASSIGNED
+    """
+    Home route serving as placeholder URL. This should be updated
+    after domains have been assigned.
+    """
+
     # return redirect(url_for("http://localhost:3000"))
     return "please visit: http://localhost:3000"
 
 
 @app.route("/api/health", methods=["GET"])
 def health():
+    """
+    Health check endpoint to ensure api is working.
+    Returns OK if the API is running.
+    """
     return "OK"
 
 
 @app.route("/api/scan-contrasting-colors", methods=["POST"])
 def scan():
+    """
+    Endpoint to scan DOM and CSS for the text color contrast accessibility.
+    Returns the color contrast score.
+    """
     data = request.get_json()
     dom = data.get("dom", "")
     css = data.get("css", "")
@@ -46,6 +58,10 @@ def scan():
 
 @app.route("/api/scan-large-text", methods=["POST"])
 def scan_large_text():
+    """
+    Endpoint to scan large text in DOM and CSS for accessibility.
+    Returns the accessibility score for large text.
+    """
     data = request.get_json()
     dom = data.get("dom", "")
     css = data.get("css", "")
