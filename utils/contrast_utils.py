ad8d3c8c74d4751c490f704dfcda99a94d337634
amelia-liston <aliston@bu.edu>
2024-10-14 15:21:55 -0400

    More pylint fixed in contrast_utils

--- utils/contrast_utils.py
+++ utils/contrast_utils.py
@@ -1,48 +1,52 @@
+import math
 import re
 from PIL import ImageColor
 from utils.debug import debug_print
-import math
 
 def css_to_hex(color):
-    """converts css color into hex"""
-    # check if valid hex
+    """
+    Converts a CSS color into a hex value.
+    """
+    # Check if it's a valid hex value
     if bool(re.match(r"^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$", color)):
         return color
 
-    # check if is valid string rgb tuple
+    # Check if it's a valid string rgb tuple
     rgb_match = re.match(
         r"^\s*rgb\s*\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)\s*$",
         color,
         re.IGNORECASE,
     )
     if rgb_match:
-        # get rgb values
+        # Get rgb values
         r, g, b = map(int, rgb_match.groups())
 
-        # check all rgb values are valid
+        # Check all rgb values are valid
         if all(0 <= x <= 255 for x in (r, g, b)):
             return rgb_to_hex((r, g, b))
 
-    # try to convert css color word (eg red) to hex
+    # Try to convert css color word (e.g., 'red') to hex
     try:
-        # css color to rgb tuple
         rgb = ImageColor.getrgb(color)
-        # tuple to hex
         return "#{:02x}{:02x}{:02x}".format(*rgb)
-    except:
+    except ValueError:
         return None
 
 
 def rgb_to_hex(rgb):
-    """converts an rgb tuple or list to its hex string.
-    assumes valid rgb input"""
-    r, g, b = [x for x in rgb]
-    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+    """
+    Converts an rgb tuple or list to its hex string.
+    Assumes valid rgb input
+    """
+    r, g, b = list(rgb)
+    return f"#{r:02x}{g:02x}{b:02x}"
 
 
 def hex_to_rgb(hex_color):
-    """converts a hexcode color into an rgb tuple.
-    assumes valid hex input"""
+    """
+    Converts a hexcode color into an rgb tuple.
+    Assumes valid hex input
+    """
     hex_color = hex_color.lstrip("#")
     if len(hex_color) == 3:
         return tuple(int(hex_color[i] + hex_color[1], 16) for i in range(3))
@@ -50,8 +54,10 @@ def hex_to_rgb(hex_color):
 
 
 def calculate_luminance(rgb: tuple[int, ...]):
-    """calculates the apparent lumunicance of the input color.
-    assumes color is rgb tuple with min 0 and max 255"""
+    """
+    Calculates the apparent lumunicance of the input color.
+    Assumes color is rgb tuple with min 0 and max 255
+    """
 
     def conv(color):
         i = float(color) / 255
@@ -60,14 +66,16 @@ def calculate_luminance(rgb: tuple[int, ...]):
         return ((i + 0.055) / 1.055) ** 2.4
 
     # https://www.w3.org/TR/WCAG20/#relativeluminancedef
+    # Calculate relative luminance
     r, g, b = [conv(x) for x in rgb]
 
     return 0.2126 * r + 0.7152 * g + 0.0722 * b
 
 
-# calculate the contrast ratio
 def contrast_ratio(rgb1: tuple[int, ...], rgb2: tuple[int, ...]):
-    """returns the contrast ratio between two colors truncated to the hundreths place"""
+    """
+    Returns the contrast ratio between two colors truncated to the hundreths place
+    """
     l_1 = calculate_luminance(rgb1)
     l_2 = calculate_luminance(rgb2)
 
